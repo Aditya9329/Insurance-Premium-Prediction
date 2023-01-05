d3b9c1191ac7089eff01e64b95d83be266263c42
Aditya9329 <maurya932926@gmail.com>
2023-01-05 08:01:24 -0800

    model_training in done

---
+++ src/feature_engineering.py
@@ -0,0 +1,13 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+def encoder_function(x_train,x_test):
+    transformer = ColumnTransformer(transformers=[
+    ('tnf1',OneHotEncoder(sparse=False,drop='first'),['sex','smoker','region'])
+    ],remainder='passthrough')
+    
+    x_train_transformed = transformer.fit_transform(x_train)
+    x_test_transformed  = transformer.transform(x_test)
+    # print(x_train_transformed)
+    # print(x_test_transformed)
+    return x_train_transformed, x_test_transformed
