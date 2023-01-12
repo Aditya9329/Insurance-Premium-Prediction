bc205a826aaaab702b9e44fd7d1069ae5227c6c1
Aditya Narayan Maurya <90122182+Aditya9329@users.noreply.github.com>
2023-01-11 18:40:50 -0800

    Update settings.py

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&9hor4h67-zo2!++c1!%=ag9nz+z6whm@$3^v-53o)u4=e*m&l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
